b05641230c10aa827412554a297db29a12ba3404
svvishjs <andreww92@live.com>
2020-09-09 22:03:25 +0100

    completed tasks 1 to 3

--- start_code/task_list.py
+++ start_code/task_list.py
@@ -5,3 +5,37 @@ tasks = [
     { "description": "Feed Cat", "completed": False, "time_taken": 5 },
     { "description": "Walk Dog", "completed": True, "time_taken": 60 },
 ]
+
+#As a user, to manage my task list I would like a program that allows me to:
+
+#1. Print a list of uncompleted tasks
+def print_uncompleted_tasks():
+    for task in tasks:
+        if task["completed"] == False:
+            print(task["description"])
+
+
+print_uncompleted_tasks()
+print("-----------------------")
+
+#2. Print a list of completed tasks
+def print_completed_tasks():
+    for task in tasks:
+        if task["completed"] == True:
+            print(task["description"])
+
+
+print_completed_tasks()
+print("-----------------------")
+
+#3. Print a list of all task descriptions
+def print_all_task_descriptions():
+    for task in tasks:
+        print(task["description"])
+
+
+print_all_task_descriptions()
+print("-----------------------")
+#4. Print a list of tasks where time_taken is at least a given time
+
+#5. Print any task with a given description
